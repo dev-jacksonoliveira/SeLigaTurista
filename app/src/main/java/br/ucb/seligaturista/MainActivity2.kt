c2afa8df48a66cef96501b8777a7e6898b46cb4c
jackend-dev <59921189+jackend-dev@users.noreply.github.com>
2021-06-14 19:21:46 -0300

    alteracao activity_main2

--- app/src/main/java/br/ucb/seligaturista/MainActivity2.kt
+++ app/src/main/java/br/ucb/seligaturista/MainActivity2.kt
@@ -12,7 +12,6 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.util.*
 
-
 class MainActivity2 : AppCompatActivity() {
 
     private var nomeDestino: EditText? = null
--- app/src/main/java/br/ucb/seligaturista/ViagemAdapter.kt
+++ app/src/main/java/br/ucb/seligaturista/ViagemAdapter.kt
@@ -14,7 +14,7 @@ class ViagemAdapter(viagemModalArrayList: ArrayList<ViagemModal>, context: Conte
     // criando uma variável para a lista de array
     private val viagemModalArrayList: ArrayList<ViagemModal> = viagemModalArrayList
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.course_rv_item, parent,
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.viagem_rv_item, parent,
             false)
         return ViewHolder(view)
     }
@@ -22,8 +22,8 @@ class ViagemAdapter(viagemModalArrayList: ArrayList<ViagemModal>, context: Conte
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // definir dados para nossas visualizações do recyclerView
         val modal: ViagemModal = viagemModalArrayList[position]
-        holder.courseNameTV.text = modal.courseName
-        holder.courseDescTV.text = modal.courseDescription
+        holder.nomeDestino.text = modal.courseName
+        holder.descricao.text = modal.courseDescription
     }
 
     override fun getItemCount(): Int {
@@ -32,8 +32,8 @@ class ViagemAdapter(viagemModalArrayList: ArrayList<ViagemModal>, context: Conte
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val courseNameTV: TextView = itemView.findViewById(R.id.idTVCourseName)
-        val courseDescTV: TextView = itemView.findViewById(R.id.idTVCourseDescription)
+        val nomeDestino: TextView = itemView.findViewById(R.id.textViewNomeDestino)
+        val descricao: TextView = itemView.findViewById(R.id.textViewDescricao)
 
         init { }
     }
